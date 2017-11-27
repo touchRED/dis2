73a9dd8a4d0bd0f932662e3622fbdd6d68ee2d6a
Connor Trotter <connor@s172-20-187-h107.paws.uga.edu>
2017-11-27 13:21:49 -0500

    port

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -34,6 +35,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(PORT, function(){
+  console.log('listening on', PORT);
 });
